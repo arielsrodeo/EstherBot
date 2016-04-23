ca518ba6fcdfa138d0c0eee5196442e949e1a22d
Ariel <ariel.zorrilla1@gmail.com>
2016-04-23 00:52:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say( 'Hey, thank you for visiting me. (You look great btw). What is your first name?')
                 .then(() => 'speak');
         }
     },
